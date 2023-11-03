f234626507afd1dda091a28ef0bdf50f604952bc
rod <rodrip0129@gmail.com>
2023-11-03 11:20:56 -0300

    add greetings controllers and update routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 Message.create!(content: "Hello, world from Argentina!")
 Message.create!(content: "Hi there!")
 Message.create!(content: "What's up guys?")
-Message.create!(content: "Hey!")
+Message.create!(content: "Hey Rod!")
 Message.create!(content: "Greetings!")
